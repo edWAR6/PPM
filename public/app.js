0da73d86337c2c2e08f42ea5acf65930a13de00e
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2017-01-24 16:52:55 -0600

    components based architecture

--- public/app.js
+++ public/app.js
@@ -1,8 +1,24 @@
 'use strict';
 
-function ppm(){
-    
+function run(){
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyCk9uz2j3CrOqPaRQacmIKEKxF0JaGV0xs",
+    authDomain: "hello-ppm.firebaseapp.com",
+    databaseURL: "https://hello-ppm.firebaseio.com",
+    storageBucket: "hello-ppm.appspot.com",
+    messagingSenderId: "595677741726"
+  };
+  firebase.initializeApp(config);
 };
 
-angular.module('ppm', [])
-.config(ppm);
+function config($routeProvider){
+  $routeProvider.when('/login', {
+    template: '<inbox mails="$resolve.mails"></inbox>',
+    resolve: {mails: function(Mails) { return Mails.fetch(); }}
+  });
+};
+
+angular.module('ppm', ['ppm.components'])
+.run(run)
+.config(config);
---
+++ public/login/login.js
@@ -0,0 +1,9 @@
+function loginController(){
+
+};
+
+angular.module('ppm.components', ['firebase'])
+.component('loginComponent', {
+    templateUrl: 'login.html',
+    controller: loginController
+});
